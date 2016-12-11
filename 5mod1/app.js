87dc0bf2b7c04635ef4f34aa07825c33d8f2c920
PJ-D <peter_jacob-dolan@bedford.k12.ma.us>
2016-12-10 20:44:03 -0500

    Update app.js

--- 5mod1/app.js
+++ 5mod1/app.js
@@ -9,12 +9,7 @@ function LunchCheckController($scope, $filter) {
   $scope.lunch = "";
   $scope.message = "";
   
-  $scope.checklunch = function () {
-  var lnch = lunch.split(',');
-  if lnch.lenght > 3 output = "Too Much!";
-    else output = "Enjoy!";
-  console.log(output);
-  return output;
+
   };
 
 
